3b6db28a4dcd33c4609eb6f18f904de54f4f092b
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-04-30 14:04:08 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,16 @@
-const redCol = () => {
-  this.style.backgroundColor = "red";
+const pinkCol = () => {
+  this.style.backgroundColor = "pink";
 };
 
 const noCol = () => {
   this.style.backgroundColor = "transparent";
 };
 
+const redCol = () => {
+  this.style.backgroundColor = "red";
+};
+
 let changeLi = document.querySelector(".filmPoster");
-changeLi.addEventListener("mouseover", redCol);
+changeLi.addEventListener("mouseover", pinkCol);
 changeLi.addEventListener("mouseleave", noCol);
-changeLi.addEventListener("onclick");
+changeLi.addEventListener("onclick", redCol);
