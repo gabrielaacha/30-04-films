b2066266ca629f8faca039919b170629f5b3875a
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-05-02 12:23:18 +0200

    fourth

---
+++ src/scripts/index.js
@@ -0,0 +1,40 @@
+const ul = document.querySelector(".filmList");
+
+function pinkCol() {
+  this.style.backgroundColor = "pink";
+}
+
+function noCol() {
+  this.style.backgroundColor = "transparent";
+}
+
+function redCol() {
+  this.style.backgroundColor = "red";
+  //   filmInfoHide.style.display = "none";
+  //   filmInfoShow.style.display = "block";
+}
+
+let changeLi = document.querySelectorAll(".filmPoster");
+changeLi.forEach((li) => {
+  li.addEventListener("mouseover", pinkCol);
+  li.addEventListener("mouseleave", noCol);
+  li.addEventListener("click", redCol);
+});
+
+ul.addEventListener("click", (e) => {
+  [...document.querySelectorAll(".filmInfo")].map((film) => {
+    film.style.display = "none";
+  });
+
+  document.querySelector(`#${e.target.id}Info`).style.display = "flex";
+  document.querySelector(`#${e.target.id}Info`).style.flexDirection = "column";
+  document.querySelector(`#${e.target.id}Info`).style.justifyContent = "right";
+});
+
+// let filmInfoHide = document.querySelector("#personaImg");
+// let filmInfoHide = document.querySelectorAll(".filmInfo");
+// filmInfoHide.forEach((sec) => {
+//   sec.addEventListener("click", redCol);
+// });
+// let filmInfoShow = document.getElementById("personaImg");
+// let filmInfoShow = document.querySelector("#personaInfo");
